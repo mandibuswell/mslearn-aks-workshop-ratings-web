dea88a806de2006bb1d8e5bfc3549357acabaa2e
mandibuswell <mandibuswell@users.noreply.github.com>
2024-03-21 23:11:00 +1300

    remove workshop wording from footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Red Hat OpenShift (ARO) workshop
+        Powered By </br> Azure Red Hat OpenShift (ARO)
       </div>
       <div class="col-lg-6">
       </div>
